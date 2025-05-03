8da1ed3829dd8046f5cdce641168013db0898a61
PiterLewis <santiago.Garcia.business@outlook.es>
2025-05-03 20:13:53 +0200

    stashed some changes

--- src/com/utad/proyectoFinal/characterSystem/tools/GenericItemType.java
+++ src/com/utad/proyectoFinal/characterSystem/tools/GenericItemType.java
@@ -2,17 +2,23 @@ package com.utad.proyectoFinal.characterSystem.tools;
 
 public enum GenericItemType {
 
-    HEALTH_POTION(20, 0),
-    MANA_POTION(0, 30),
-    UPGRADE_HEALTH_POTION(40,0),
-    UPGRADE_MANA_POTION(0, 40);
+    HEALTH_POTION("Health potion", 20, 0),
+    MANA_POTION("Mana potion", 0, 30),
+    UPGRADE_HEALTH_POTION("Upgrade health potion", 40,0),
+    UPGRADE_MANA_POTION("Upgrade mana potion", 0, 40);
 
+    private final String name;
     private final int healthRegen;
     private final int manaRegen;
 
-    GenericItemType(int healthRegen, int manaRegen) {
+    GenericItemType(String name, int healthRegen, int manaRegen) {
+        this.name = name;
         this.healthRegen = healthRegen;
         this.manaRegen = manaRegen;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public int getHealthRegen() {
